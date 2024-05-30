a5ef5407b7ad880a4df5cf5306e114fa94dd7b4e
drabenstadtj <jackdrabenstadt@gmail.com>
2024-05-29 22:59:32 -0400

    same

--- backend/app/routes/transactions.py
+++ backend/app/routes/transactions.py
@@ -1,37 +1,27 @@
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, request, jsonify
+from flask_cors import CORS
 from app.services.transaction_service import TransactionService
 
 # Create a Blueprint for transaction-related routes
 bp = Blueprint('transactions', __name__, url_prefix='/transactions')
+CORS(bp, supports_credentials=True)  # Apply CORS to the blueprint
 
 @bp.route('/buy', methods=['POST'])
 def buy_stock():
-    if 'user_id' not in session:
-        return jsonify({"error": "User not logged in"}), 401
-
     data = request.get_json()
-    data['user_id'] = session['user_id']
+    data['user_id'] = None #USER ID
     result = TransactionService.buy_stock(data)
     return jsonify(result)
 
 @bp.route('/sell', methods=['POST'])
 def sell_stock():
-    if 'user_id' not in session:
-        return jsonify({"error": "User not logged in"}), 401
-
     data = request.get_json()
-    print(data)
-    data['user_id'] = session['user_id']
+    data['user_id'] = None #USER ID
     result = TransactionService.sell_stock(data)
     return jsonify(result)
 
 @bp.route('/', methods=['GET'])
 def get_transactions():
-    """
-    Fetch transactions.
-    Optionally expects a query parameter 'user_id' to fetch transactions for a specific user.
-    Returns a list of transactions.
-    """
     user_id = request.args.get('user_id')
     transactions = TransactionService.get_transactions(user_id)
     return jsonify(transactions)
