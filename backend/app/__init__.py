63c1077d9f30c67540d53cc6c0136c6def9afc07
drabenstadtj <jackdrabenstadt@gmail.com>
2024-06-09 20:31:01 -0400

    enabled cors debugging

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,7 +11,10 @@ def create_app(config_name='prod'):
     app.config.from_object(config_by_name[config_name])
 
     # Setup CORS using the configuration
+    logging.basicConfig(level=logging.INFO)
     cors_origins = app.config['CORS_ORIGINS'].split(',')
+    app.logger.log(msg=cors_origins, level=logging.INFO)
+
     CORS(app, resources={r"/*": {"origins": cors_origins}})
 
     # Initialize PyMongo
